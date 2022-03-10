ebd6c3b9d0e5e070871b9db4ab8df82a3c11482a
caro-mugo <mugonycaroline@gmail.com>
2022-03-10 08:57:08 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+
+    var y=getMode(7,4)
+    println(y)
+    var z=getAnswer("caroline",22)
+    println(z)
+    var x=getSolution("vitz")
+    println{x}
+
+}
+fun getMode(a:Int,b:Int):Int{
+   var mode=a%b
+    return mode
+
+
+}
+fun getAnswer(name:String,age:Int):String{
+    var answer="Hi my name is caroline"+ " " + "and am 22 years old"
+    return answer
+
+}
+fun getSolution(name:String):String{
+    var car="vitz"
+    println(name.length)
+    return car
+}
+
